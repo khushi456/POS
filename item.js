00a39eea56bfdb80b26838465af48dff6bcea317
Khushi Shivde <48391989+khushi456@users.noreply.github.com>
2019-12-19 21:11:29 +0530

    Added more features on existing files.

---
+++ item.js
@@ -0,0 +1,24 @@
+console.log("oyee anuu sunnnnn");
+
+function myyy() {
+    $('#one').show();
+    $('#two').show();
+
+    $('#demo').hide();
+    // $('#fish').hide();
+    // $('#soups').hide();
+    // $('#desserts').hide();
+    // $('#desserts').hide();
+    // $('#desserts').hide();
+
+}
+// function fish()
+// {
+
+// }
+
+function myFunction1() {
+    $('#one').hide();
+    $('#two').hide();
+
+}
